e084536b69e149e4826dd4e012bf0f0a793deeaa
rggamiao <rejicamiling@gmail.com>
2024-10-03 16:58:52 -0700

    Fixed code

--- functions_practice.py
+++ functions_practice.py
@@ -1,9 +1,55 @@
-def max_num(a, b, c):
-    if a > b and a > c:
-        return a
-    elif b > a and b > c:
-        return b
-    else:
-        return c
-    
-print(max_num(25, 38, 17)) #expects 38
+#
+
+# def hello():
+  #  print("Hello! Welcome to my program!")
+
+#def pack(arg1, arg2, arg3):
+    #return [arg1, arg2, arg3]
+
+#def eat_lunch(lunch_items):
+    #if not lunch_items:
+     #   print("My lunchbox is empty!")
+    #else:
+     #   print(f"First I eat {lunch_items[0]}")
+      #  for item in lunch_items[1:]:
+       #     print(f"Next I eat {item}")
+
+# edit this, syntax error
+#hello()
+
+#packed_list = pack('apple', 'sandwich', 'juice')
+#print(packed_list)
+
+#eat_lunch(packed_list)
+#eat_lunch([]) #
+
+# A function named hello() that prints a greeting to the user. This function should accept no arguments and returns nothing.
+
+def hello():
+  print("Hello, user!")
+  
+# A function named pack() that accepts three arguments, and returns a single list with the three arguments inside as list elements.
+
+def pack(a,b,c):
+  return [a,b,c]
+
+# A function called eat_lunch(). This function should accept a list of any length, and print out a series of strings that say "First I eat __" 
+# (the first element of the list), and "Next I eat ___" (for the following elements in the list). If the list is empty, print "My lunchbox is empty!". 
+# The function should not return anything.
+
+def eat_lunch(my_lst):
+  if len(my_lst) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(my_lst)):
+      if i == 0:
+        print(f"First I eat {my_lst[0]}")
+      else:
+        print(f"Next I eat {my_lst[i]}")
+
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
